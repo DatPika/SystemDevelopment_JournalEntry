e0685dcfc31aacec396f583adcc0e82a1f0b4759
DatPika <levitind@gmail.com>
2023-09-10 01:49:50 -0400

    script so far added
    TODO: remove powershell command so it's fully cmd

---
+++ journal_entry.bat
@@ -0,0 +1,23 @@
+@echo off
+:: The goal of this script is to create a new personal journal entry for the System Development course.
+
+:: Planning out the code
+:: [Today's date] > journal_entry.txt
+:: add more content/formatting
+:: have variable for amount of template entries -> use for loop using variable given
+:: assuming %1 gets it as string, transfer into variable with set /A no-entries=%1
+:: (/A is for when value needs to be numeric)
+
+:: Print DATE (already formatted well) and start new file
+echo %DATE%: > journal_entry.txt
+echo filler_text_here. >> journal_entry.txt
+:: Create variable month-short and set it to ...
+::Set month-short=Jan. Feb. Mar. Apr. Ma. Jun. Jul. Aug. Sept. Oct. Nov. Dec.
+::echo %month-short% >> journal_entry.txt
+:: To explain...
+for /f %%i in ('powershell ^(get-date^).DayOfWeek') do (set dow=%%i)
+:: Add day of week to the file
+echo %dow%,  >> journal_entry.txt
+powershell -command "{get-date -f 'dddd MMM. dd, xx:xx \to hh:mm (x \h xx \min - a\t \ho\me):'}" >> journal_entry.txt
+:: Opens the file
+start notepad journal_entry.txt
